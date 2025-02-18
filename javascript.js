f5ec189b423c953b9e07bde042d2861c53e08764
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-18 16:13:15 -0600

    initial code, functions, page layout

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -14,4 +16,60 @@ function Book(title, author, pages, read) {
     };
 }
 
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function refreshLibrary() {
+    const shelf = document.querySelector(".shelf");
+
+    while (shelf.firstChild) {
+        shelf.removeChild(shelf.firstChild);
+    }
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        const book = document.createElement('div');
+        book.textContent = myLibrary[i].info();
+        shelf.appendChild(book);
+    }
+}
+
+function saveBook(event) {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = parseInt(document.querySelector("#pages").value);
+    const readElement = document.getElementsByName("read");
+
+    if (readElement[0].checked) {
+        read = true;
+    } else {
+        read = false;
+    }
+
+    const newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+    refreshLibrary;
+
+    event.preventDefault();
+}
+
+function clearForm() {
+    document.querySelector("#title").value = '';
+    document.querySelector("#author").value = '';
+    document.querySelector("#pages").value = '';
+    document.getElementsByName("read")[0].checked = true;
+}
+
+const buttonRefresh = document.querySelector("#buttonRefresh");
+const buttonSave = document.querySelector("#buttonSave");
+const buttonClear = document.querySelector('#buttonClear');
+
+buttonSave.addEventListener("click", saveBook, false);
+buttonRefresh.addEventListener('click', refreshLibrary);
+buttonClear.addEventListener('click', clearForm);
+
 const theHobbit = new Book('The Hobbit','J.R.R. Tolkien',295,false);
+const book2 = new Book('Book Two','Mr. Author',100,true);
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(book2);
