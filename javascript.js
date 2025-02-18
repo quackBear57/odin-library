9379329274fdc163c8b968dc51c45a35ffaef2fc
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-18 16:38:25 -0600

    work on save book and refresh library fncs

--- javascript.js
+++ javascript.js
@@ -1,17 +1,18 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, shelfIndex) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.shelfIndex = shelfIndex;
     this.info = function() {
         if (this.read == false) {
             return (`${this.title} by ${this.author}, ${pages}  pages, ` + 
-            `not read yet`)
+            `not read yet, index ${this.shelfIndex}`)
         } else {
             return (`${this.title} by ${this.author}, ${pages}  pages, ` + 
-                `alredy read`)
+                `alredy read, index ${this.shelfIndex}`)
         };
     };
 }
@@ -22,15 +23,27 @@ function addBookToLibrary(book) {
 
 function refreshLibrary() {
     const shelf = document.querySelector(".shelf");
-
+    
     while (shelf.firstChild) {
         shelf.removeChild(shelf.firstChild);
     }
 
     for (let i = 0; i < myLibrary.length; i++) {
-        const book = document.createElement('div');
-        book.textContent = myLibrary[i].info();
-        shelf.appendChild(book);
+        const bookDiv = document.createElement('div');
+        bookDiv.classList = "book";
+        const buttonRead = document.createElement('button');
+        const buttonDelete = document.createElement('button');
+
+        buttonRead.addEventListener('click', () => {
+            console.log(`${this.shelfIndex}`);
+        });
+        buttonRead.textContent = "Read/Unread";
+        
+        const bookText = document.createElement('div');
+        bookText.textContent = myLibrary[i].info();
+        bookDiv.appendChild(bookText);
+        bookDiv.appendChild(buttonRead);
+        shelf.appendChild(bookDiv);
     }
 }
 
@@ -39,6 +52,7 @@ function saveBook(event) {
     const author = document.querySelector("#author").value;
     const pages = parseInt(document.querySelector("#pages").value);
     const readElement = document.getElementsByName("read");
+    const shelfIndex = myLibrary.length;
 
     if (readElement[0].checked) {
         read = true;
@@ -46,9 +60,10 @@ function saveBook(event) {
         read = false;
     }
 
-    const newBook = new Book(title, author, pages, read);
+    const newBook = new Book(title, author, pages, read, shelfIndex);
     addBookToLibrary(newBook);
     refreshLibrary;
+    clearForm;
 
     event.preventDefault();
 }
@@ -68,8 +83,7 @@ buttonSave.addEventListener("click", saveBook, false);
 buttonRefresh.addEventListener('click', refreshLibrary);
 buttonClear.addEventListener('click', clearForm);
 
-const theHobbit = new Book('The Hobbit','J.R.R. Tolkien',295,false);
-const book2 = new Book('Book Two','Mr. Author',100,true);
-
+const theHobbit = new Book('The Hobbit','J.R.R. Tolkien',295,false, myLibrary.length);
 addBookToLibrary(theHobbit);
+const book2 = new Book('Book Two','Mr. Author',100,true, myLibrary.length);
 addBookToLibrary(book2);
